df01ab1ad23f1036b09b5486c4278ae00b9c3a6b
grv99 <grviitr@gmail.com>
2013-11-27 11:12:30 +0530

    Added CSS

--- js/main.js
+++ js/main.js
@@ -1,80 +1,108 @@
-$(document).ready(function() {
-	testFunc();
-});
 
-function testFunc() {
-	$('#test-div').html("sjfngsj");
-}
-
-function getUpdates(url){
+function getPublicStream () {
+	url = "http://api.flickr.com/services/feeds/photos_public.gne?lang=en-us&format=json&jsoncallback=?";
+	
 	$.getJSON(url, function(data) {
+	
+		if(data == null || data.items == 0) {
+			return noData();
+		}
+
+		$('#feeds-div').html("<ul>");
 		$.each(data.items, function(i, item) {
 			img = item.media.m;
 			author = getAuthor(item.author);
-			element = "<img src=" + img + "></img><div class=\"author\"><a href=\"userpage.html?id=" + item.author_id + "\">" + author +"</a></div><br />";
-			$('#feeds-div').prepend(element);
+			element = "<li class=\"span5 well\">"
+						+ "<img src="+img+" class=\"img-polaroid img-rounded\" />"
+						+ "<a href=\"userpage.html?id=" + item.author_id + "\">"
+						+ "<div class=\"author pull-right\">" + author + "</div>"
+						+ "</a>"
+						+ "</li>";
+			$('#feeds-div').append(element);
+			sessionStorage.setItem(item.author_id, author);
 		});
+		$('#feeds-div').append("</ul>");
 	});
 }
 
-function getAuthor(authorName) {
-	pattern = /nobody@flickr.com \((.+)\)/;
-	//pattern = /nobody@flickr.com \((\w+)\)/;
-	matched = authorName.match(pattern);
-	if(matched == null) {
-		// some names couldn't be read
-		return "Unknown";
-	}
-	return matched[1];
-}
 
 function getUserPhotos () {
 	path = $(location).attr('href');
 	userid = path.split("=")[1];
-	//userid = userid[1];
 	url = "http://api.flickr.com/services/feeds/photos_public.gne?id="+ userid +"&lang=en-us&format=json&jsoncallback=?";
-	//getUpdates(url);
-
-	$('#user-details').html("Put user details here.");
-	$('#friends-feed-link').html("<a href=\"friends-feed.html?id="+ userid +"\">Friends feed</a>");
+	
+	whoseFeed = sessionStorage.getItem(userid) + "'s feed";
+	$('#user').html(whoseFeed);
+	document.title = whoseFeed;
+	$('#friends-feed-link').html("<a href=\"friends-feed.html?id="+ userid +"\">friends' feed &rarr;</a>");
 
 	$.getJSON(url, function(data) {
+	
+		if(data == null || data.items == 0) {
+			return noData();
+		}
+		
+		$('#feeds-div').html("<ul>");
 		$.each(data.items, function(i, item) {
 			img = item.media.m;
-			element = "<img src=" + img + "></img><br />";
-			$('#feeds-div').prepend(element);
+			element = "<li class=\"span5 well\">"
+						+ "<img src="+img+" class=\"img-polaroid img-rounded\" />"
+						+ "</li>";
+			$('#feeds-div').append(element);
 		});
+		$('#feeds-div').append("</ul>");
 	});
 }
+
 
 function getFriendsStream () {
 	path = $(location).attr('href');
-	userid = path.split("=");
-	userid = userid[1];
+	userid = path.split("=")[1];
 	url = "http://api.flickr.com/services/feeds/photos_friends.gne?user_id="+ userid +"&lang=en-us&format=json&jsoncallback=?";
-	//getUpdates(url);
-
+	
+	whoseFeed = sessionStorage.getItem(userid) + " friends' feed";
+	$('#user').html(whoseFeed);
+	document.title = whoseFeed;
+	
 	$.getJSON(url, function(data) {
+	
+		if(data == null || data.items == 0) {
+			return noData();
+		}
+
+		$('#feeds-div').html("<ul>");
 		$.each(data.items, function(i, item) {
 			img = item.media.m;
 			author = getAuthor(item.author);
-			element = "<img src=" + img + "></img><div class=\"author\"><a href=\"userpage.html?id=" + item.author_id + "\">" + author +"</a></div><br />";
-			$('#feeds-div').prepend(element);
+			element = "<li class=\"span5 well\">"
+						+ "<img src="+img+" class=\"img-polaroid img-rounded\" />"
+						+ "<a href=\"userpage.html?id=" + item.author_id + "\">"
+						+ "<div class=\"author pull-right\">" + author + "</div>"
+						+ "</a>"
+						+ "</li>";
+			$('#feeds-div').append(element);
+			sessionStorage.setItem(item.author_id, author);
 		});
+		$('#feeds-div').append("</ul>");
 	});
 }
 
-function getPublicStream () {
-	url = "http://api.flickr.com/services/feeds/photos_public.gne?lang=en-us&format=json&jsoncallback=?";
-	//getUpdates(url);
 
-	$.getJSON(url, function(data) {
-		$.each(data.items, function(i, item) {
-			img = item.media.m;
-			author = getAuthor(item.author);
-			element = "<img src=" + img + "></img><div class=\"author\"><a href=\"userpage.html?id=" + item.author_id + "\">" + author +"</a></div><br />";
-			$('#feeds-div').prepend(element);
-		});
-	});
+function getAuthor(authorName) {
+	pattern = /nobody@flickr.com \((.+)\)/;
+	matched = authorName.match(pattern);
+	if(matched == null) {
+		// some names couldn't be read
+		return "";
+	}
+	return matched[1];
+}
 
+function noData() {
+	element = "<div class=\"alert\"> <big>Sorry. This place is empty :( </big></div>"
+				+ "<div style=\"text-align:center;\">"
+				+ "<button class=\"btn\" onclick=\"javascript:history.back();\" type=\"button\">&larr; Go Back</button>"
+				+ "</div>";
+	$('#feeds-div').html(element);
+	return false;
 }
